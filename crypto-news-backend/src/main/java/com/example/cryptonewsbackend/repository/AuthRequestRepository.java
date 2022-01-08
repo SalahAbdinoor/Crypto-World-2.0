7bd574620d6ceaa64aa90a5e7cfcd83efa35152c
Salah Abdinoor <Salah.abdinoor@hotmail.com>
2022-01-08 17:35:24 +0100

    change comp

---
+++ crypto-news-backend/src/main/java/com/example/cryptonewsbackend/repository/AuthRequestRepository.java
@@ -0,0 +1,8 @@
+package com.example.cryptonewsbackend.repository;/**
+ * Created by Salah Abdinoor
+ * 1/8/2022
+ * 5:29 PM
+ * crypto-news-backend
+ * Copyright: MIT
+*/ public class AuthRequestRepository {
+}
